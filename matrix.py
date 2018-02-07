0a69e494df289ffe551c94b0b8974c0b920dc9ed
hoanglaszlo <hoanglaszlo@gmail.com>
2018-02-07 22:50:04 +0100

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,5 +1,8 @@
 import types
 
+class Matrix_Error(Exception):
+	pass
+
 class Matrix:
 	def __init__(self, *args):
 		if not (len(args) == 1 or len(args) == 2):
@@ -45,15 +48,22 @@ class Matrix:
 
 
 class NumberSystem:
-	def __init__(self, matrix):
+	def __init__(self, matrix, digitSet):
 		self.matrix = matrix
+		self.digitSet = digitSet
 
 	def isCongruent(self, elementOne, elementTwo):
 		G, U, V = m.smith_form()
 		return self.hashFunction(U*elementOne, G) == self.hashFunction(U*elementTwo, G)
 
 	def hashFunction(self, U, G):
-		return sum((U[i] % G[i,i])*prod(G[j,j] for j in range(i)) for i in range(len(U)))
+		return sum((U[i] % G[i,i])*prod(G[j,j] for j in range(i)) for i in range(rank(U)))
+
+	def isCompleteResidues(self):
+		for i in self.digitSet:
+			if any(self.isCongruent(i,j) for j in self.digitSet-{i}):
+				return false
+		return true
 
 	def isExpansive(self):
 		eigens = self.matrix.eigenvalues()
@@ -64,8 +74,6 @@ class NumberSystem:
 		return abs(tmp.determinant()) != 1
 
 m = Matrix([[10]])
-v1 = vector([23])
-v2 = vector([2])
 
-numsys = NumberSystem(m)
-numsys.isCongruent(v1, v2)
+numsys = NumberSystem(m, {0,1,2,3,4,5,6,7,8,9})
+numsys.isCompleteResidues()
