9095a7fc1f3938760a011672c4809488e07c16fe
hoanglaszlo <hoanglaszlo@gmail.com>
2018-02-13 08:28:02 +0100

    Added testing framework

--- matrix.py
+++ matrix.py
@@ -1,29 +1,43 @@
 import types
 import sys
 import random
+import operator
+import unittest
 
 class MatrixError(Exception):
 	pass
 
 class MatrixArithmeticError(MatrixError):
-	def __init__(self, a, b, operation):
-		self.a = a
-		self.b = b
+	def __init__(self, elementLeft, elementRight, operation):
+		self.elementLeft = elementLeft
+		self.elementRight = elementRight
 		self.operation = operation
 
 	def __str__(self):
-		return "Cannot %s a %dx%d and a %dx%d matrix" % (self.operation, self.a.rows(), self.a.cols(), self.b.rows(), self.b.cols())
+		return "Cannot %s a %dx%d and a %dx%d matrix" % (self.operation, self.elementLeft.rows(), self.elementLeft.cols(), self.elementRight.rows(), self.elementRight.cols())
 
 class MatrixAdditionError(MatrixArithmeticError):
-	def __init__(self, a, b):
-		MatrixArithmeticError.__init__(self, a, b, "add")
+	def __init__(self, elementLeft, elementRight):
+		MatrixArithmeticError.__init__(self, elementLeft, elementRight, "add")
+
+class MatrixMultiplicationError(MatrixArithmeticError):
+	def __init__(self, elementLeft, elementRight):
+		MatrixArithmeticError.__init__(self, elementLeft, elementRight, "multiply")
 
 class SquareError(MatrixError):
-	def __init__(self, msg):
-		self.msg = msg
+	def __init__(self, function):
+		self.function = function
 
 	def __str__(self):
-		return "%s only defined for square matricies." % self.msg
+		return "The %s function is only defined for square matricies." % self.function
+
+class DeterminantError(SquareError):
+	def __init__(self):
+		SquareError.__init__(self, "determinant")
+
+class InverseError(SquareError):
+	def __init__(self):
+		SquareError.__init__(self, "inverse")
 
 class Matrix:
 	def __init__(self, *args):
@@ -56,7 +70,7 @@ class Matrix:
 		
 	def __add__(self, other):
 		if not isinstance(other, Matrix):
-			raise TypeError("Cannot add a matrix to type %s" % type(other))
+			raise TypeError("Cannot add a matrix and a %s" % type(other))
 		if not (self.cols() == other.cols() and self.rows() == other.rows()):
 			raise MatrixAdditionError(self, other)
 		r = []
@@ -73,41 +87,75 @@ class Matrix:
 		return -1 * self
 
 	def __mul__(self, other):
-		if self.isScalarElement(other):
-			return self.scalarMultiply(other)
+		if self.is_scalar_element(other):
+			return self.scalar_multiply(other)
 		if not isinstance(other, Matrix):
 			raise TypeError("Cannot multiply matrix and type %s" % type(other))
-		#if other.is_row_vector():
-		#	raise MatrixMultiplication_Error(self, other)
-		#return self.matrix_multiply(other)
+		return self.matrix_multiply(other)
 
 	def __rmul__(self, other):
-		if not self.isScalarElement(other):
+		if not self.is_scalar_element(other):
 			raise TypeError("Cannot right-multiply by %s" % type(other))
-		return self.scalarMultiply(other)
+		return self.scalar_multiply(other)
 
-	def scalarMultiply(self, scalar):
+	def __eq__(self, other):
+		if not isinstance(other, Matrix):
+			raise TypeError("Cannot equal a matrix and a %s" % type(other))
+		return all(self.row(i) == other.row(i) for i in xrange(self.rows()))
+
+	def scalar_multiply(self, scalar):
 		rows = []
 		for row in self.matrix:				
 			rows.append(map(lambda x: x * scalar, row))
 		return Matrix(rows)
 
-	def isScalarElement(self, x):
+	def matrix_multiply(self, other):
+		r = []
+		if not isinstance(other, Matrix):
+			raise TypeError("Cannot multiply matrix and type %s" % type(other))
+		if not self.cols() == other.rows():
+			raise MatrixMultiplicationError(self, other)
+		for row in xrange(self.rows()):
+			r.append([])
+			for col in xrange(other.cols()):
+				r[row].append(self.vector_inner_product(self.row(row), other.col(col)))
+		if len(r) == 1 and len(r[0]) == 1:
+			return r[0][0]
+		else:
+			return Matrix(r)
+
+	def vector_inner_product(self, a, b):
+		if not isinstance(a, types.ListType):
+			raise TypeError("Only two lists are accepted.")
+		if not isinstance(b, types.ListType):
+			raise TypeError("Only two lists are accepted.")
+		return reduce(operator.add, map(operator.mul, a, b))
+
+	def is_scalar_element(self, x):
 		return isinstance(x, types.IntType) or isinstance(x, types.FloatType) or isinstance(x, types.ComplexType)
 
-	def isRowVector(self):
+	def is_row_vector(self):
 		return self.rows() == 1 and self.cols() > 1
 
-	def isColumnVector(self):
+	def is_column_vector(self):
 		return self.cols() == 1 and self.rows() > 1
-		
+	
+	def row(self, i):
+		return self.matrix[i]
+
+	def col(self, j):
+		r = []
+		for row in self.matrix:
+			r.append(row[j])
+		return r
+
 	def rows(self):
 		return len(self.matrix)
 
 	def cols(self):
 		return len(self.matrix[0])
 		
-	def isSquare(self):
+	def is_square(self):
 		return self.rows() == self.cols()
 		
 	def zeros(self, row, col):
@@ -147,14 +195,14 @@ class Matrix:
 			return (self.matrix[0][0] * self.matrix[1][1] - self.matrix[0][1] * self.matrix[1][0])
 
 	@classmethod
-	def makeRandom(cls, m, n, low=0, high=10):
+	def make_random(cls, m, n, low=0, high=10):
 		rows = []
 		for x in range(m):
 			rows.append([random.randrange(low, high) for i in range(n)])
 		return Matrix(rows)
 
 	@classmethod
-	def readConsole(cls):
+	def read_console(cls):
 		print 'Enter matrix row by row. Type "q" to quit.'
 		rows = []
 		while True:
@@ -165,7 +213,7 @@ class Matrix:
 		return Matrix(rows)
 
 	@classmethod
-	def readFile(cls, fname):
+	def read_file(cls, fname):
 		rows = []
 		for line in open(fname).readlines():
 			row = [int(number) for number in line.split()]
@@ -179,42 +227,79 @@ class Matrix:
 			matrix[(index, index)] = 1
 		return matrix
 
-
 class NumberSystem:
 	def __init__(self, matrix, digitSet):
+		if not matrix.is_square():
+			raise DeterminantError()
+		if matrix.determinant() == 0:
+			raise InverseError()
+		#self.lattice = lattice
 		self.matrix = matrix
 		self.digitSet = digitSet
 
-	def hashFunction(self, U, G):
-		return sum((U[i] % G[i,i])*prod(G[j,j] for j in range(i)) for i in range(rank(U)))
+	def hash_function(self, U, G):
+		#TODO: find s pos. int. Is it necessary?
+		return sum((U[i] % G[i,i]) * prod(G[j,j] for j in range(i)) for i in range(rank(U)))
 
-	def isCongruent(self, elementOne, elementTwo):
+	def is_congruent(self, elementOne, elementTwo):
 		G, U, V = m.smith_form()
-		return self.hashFunction(U*elementOne, G) == self.hashFunction(U*elementTwo, G)
+		return self.hash_function(U*elementOne, G) == self.hash_function(U*elementTwo, G)
 
-	def isCompleteResidues(self):
+	def is_complete_residues_system(self):
 		for i in self.digitSet:
-			if any(self.isCongruent(i,j) for j in self.digitSet-{i}):
+			if any(self.is_congruent(i,j) for j in self.digitSet - {i}):
 				return false
 		return true
 
-	def isExpansive(self):
+	def is_expansive(self):
 		eigens = self.matrix.eigenvalues()
 		return all((abs(i)>1) for i in eigens)
 
-	def unitCondition(self):
+	def unit_condition(self):
 		n = self.matrix.rows()
 		tmp = self.matrix.identity(n) - self.matrix
 		return abs(tmp.determinant()) != 1
 
 	def check(self):
-		if self.isExpansive() and self.unitCondition() and self.isCompleteResidues():
+		if self.is_expansive() and self.unit_condition() and self.is_complete_residues_system():
 			return true
 		return false
 
-mat = Matrix([[6,1,1],[4,-2,5],[2,8,7]])
-id = Matrix.identity(3)
-digitSet = {0,1,2,3,4,5,6,7,8,9}
+class MatrixTests(unittest.TestCase):
+	def testAdd(self):
+		m1 = Matrix([[1, 2, 3], [4, 5, 6]])
+		m2 = Matrix([[7, 8, 9], [10, 11, 12]])		
+		m3 = m1 + m2
+		self.assertTrue(m3 == Matrix([[8, 10, 12], [14,16,18]]))
 
-numsys = NumberSystem(mat, digitSet)
-print numsys.unitCondition()
+	def testSub(self):
+		m1 = Matrix([[1, 2, 3], [4, 5, 6]])
+		m2 = Matrix([[7, 8, 9], [10, 11, 12]])		
+		m3 = m2 - m1
+		self.assertTrue(m3 == Matrix([[6, 6, 6], [6, 6, 6]]))
+
+	def testMul(self):
+		m1 = Matrix([[1, 2, 3], [4, 5, 6]])
+		m2 = Matrix([[7, 8], [10, 11], [12, 13]])
+		self.assertTrue(m1 * m2 == Matrix([[63, 69], [150, 165]]))
+		self.assertTrue(m2 * m1 == Matrix([[39, 54, 69], [54, 75, 96], [64, 89, 114]]))
+		
+class NumberSystemTests(unittest.TestCase):
+	def testUnitCondition(self):
+		mat = Matrix([[6,1,1],[4,-2,5],[2,8,7]])
+		digitSet = {0,1,2,3,4,5,6,7,8,9}
+
+		numsys = NumberSystem(mat, digitSet)
+		l = numsys.unit_condition()
+		self.assertTrue(l == True)
+		
+	def testUnitCondition_2(self):
+		mat = Matrix([[-1,-1],[1,-1]])
+		digitSet = {(0,0),(1,0)}
+
+		numsys = NumberSystem(mat, digitSet)
+		l = numsys.unit_condition()
+		self.assertTrue(l == True)
+
+if __name__ == "__main__":
+	unittest.main()
